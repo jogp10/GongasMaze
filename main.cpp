933634b0a816289738c86e31e31e9ad0551ca5d7
jogp10 <jogp10@gmail.com>
2021-03-31 19:47:52 +0100

    main menu

--- main.cpp
+++ main.cpp
@@ -13,7 +13,7 @@ Let's play
 void play();
 
 int main() {
-    int choice;
+    int menu_choice;
     int goBack;
     string line;
     ifstream myfile ("/home/joao/Documents/c++/T02_G11/rules.txt");
@@ -22,9 +22,9 @@ int main() {
     cout << "1) Rules" << endl;
     cout << "2) Play" << endl;
     cout << "0) Exit" << endl;
-    cin >> choice;
+    cin >> menu_choice;
 
-    // If the choice is not an integer
+    // If the menu_choice is not an integer
     if(cin.fail()){
         cin.clear();
         cin.ignore(10000, '\n');
@@ -32,7 +32,7 @@ int main() {
         return main();
     }
 
-    switch(choice)
+    switch(menu_choice)
     {
         case 0:     //Exit Game
             break;
@@ -50,7 +50,8 @@ int main() {
         case 2:    // Start the game
             play();
             break;
-        default:  // If the choice is neither 0, 1 or 2
+
+        default:  // If the menu_choice is neither 0, 1 or 2
             cout << "Invalid choice! Please choose between 0, 1 and 2!" << endl;
             return main();
     }
