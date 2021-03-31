0a8872e0780924fa0cb2b09e59019a3656fd34d2
jogp10 <jogp10@gmail.com>
2021-03-31 19:22:53 +0100

    maze

--- main.cpp
+++ main.cpp
@@ -2,15 +2,17 @@
 
 #include <iostream>
 
-/**
-Escrever funçoes em development.cpp
-Meter nome, parâmetros e breve descrição do seu uso
-Chamar em main
-*/
-
 using namespace std;
 
+/**
+Start of the game
+Start menu, player can choose from 0 to 2. 0:Exit, 1:Rules, 2:Play
+ */
+void menu();
+void rules();
+
 int main() {
+    menu();
 
     return 0;
 }
