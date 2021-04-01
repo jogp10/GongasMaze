d3d8ad8ddb3c665cd5d2f6a1be458f4a2b7f7ca9
jogp10 <jogp10@gmail.com>
2021-04-01 11:47:25 +0100

    limpar o file

--- main.cpp
+++ main.cpp
@@ -5,50 +5,50 @@
 #include <string>
 #include <thread>
 #include <chrono>
+#include <iomanip>
 
 using namespace std;
+
+/**
+Main menu
+ */
+int menu();
+
+
+/**
+Rules of the game
+ */
+void ReadRules();
+
 
 /**
 Start of the game
 Let's play
  */
 void play();
-void winner(int name, int time, int maze);
+
+
+/**
+ * Write winner's name and time in record
+ * @param name of the player
+ * @param time of the play
+ * @param maze in which level
+ */
+void winner(string name, int time, int maze);
+
 
 int main() {
-    winner(120, 120, 1);
-
+    winner("ze", 120, 1);
     int menu_choice;
-    int goBack;
-    string line;
-    ifstream myfile ("/home/joao/Documents/c++/T02_G11/rules.txt");
 
-    // Main Menu
-    cout << "1) Rules" << endl;
-    cout << "2) Play" << endl;
-    cout << "0) Exit" << endl;
-    cin >> menu_choice;
-
-    // If the menu_choice is not valid
-    while(cin.fail() || (menu_choice!=0 && menu_choice !=1 && menu_choice != 2)){
-        cin.clear();
-        cin.ignore(10000, '\n');
-        cerr << "Input a valid operation! (0, 1 or 2 to proceed)" << endl;
-        cin >> menu_choice;
-    }
+Menu:
+    menu_choice = menu();
 
     switch(menu_choice)
     {
         case 1:     // Rules of the game
-            while ( getline (myfile,line) )
-            {
-                cout << line << '\n';
-            }
-            myfile.close();
-            cout << "Press '0' to go to the main menu" << endl;
-            cin >> goBack;
-            while(goBack != 0) cin >> goBack;
-            return main();
+            ReadRules();
+            goto Menu;
 
         case 2:    // Start the game
             play();
@@ -60,8 +60,4 @@ int main() {
     }
 
     return 0;
-}
-
-void play(){
-
 }
--- play.cpp
+++ play.cpp
@@ -3,11 +3,59 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <thread>
+#include <chrono>
 #include <iomanip>
 
 using namespace std;
 
-void winner(int name,int time,int maze){
+int menu(){
+    int menu_choice;
+
+    // Main Menu
+    cout << "1) Rules" << endl;
+    cout << "2) Play" << endl;
+    cout << "0) Exit" << endl;
+    cin >> menu_choice;
+
+    // If the menu_choice is not valid
+    while(cin.fail() || (menu_choice!=0 && menu_choice !=1 && menu_choice != 2)){
+        cin.clear();
+        cin.ignore(10000, '\n');
+        cerr << "Input a valid operation! (0, 1 or 2 to proceed)" << endl;
+        cin >> menu_choice;
+    }
+    return menu_choice;  // return choice
+}
+
+
+void ReadRules(){
+    string line;
+    ifstream myfile ("/home/joao/Documents/c++/T02_G11/rules.txt");
+    int goBack;
+
+    // Open file and print all the lines
+    if(myfile.is_open()) {
+        while (getline(myfile, line)) {
+            cout << line << '\n';
+        }
+        myfile.close();
+    }
+
+    //Go back to main menu by pressing '0'
+    cout << "Press '0' to go to the main menu" << endl;
+    cin >> goBack;
+
+    while(goBack != 0 || cin.fail())
+    {
+        cin.clear();
+        cin.ignore(10000, '\n');
+        cin >> goBack;
+    }
+}
+
+
+void winner(string name,int time,int maze){
     string path= "/home/joao/Documents/c++/T02_G11/Maze/MAZE_XX_WINNERS.TXT";
     path[43] = (char)(maze/10+'0');
     path[44] = (char)(maze%10+'0');
@@ -20,3 +68,7 @@ void winner(int name,int time,int maze){
     win.close();
 }
 
+
+void play(){
+    cout << "Done!" << endl;
+}
